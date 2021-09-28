26ed19546501bdab1d9a845d20985bd640698811
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-09-27 21:02:53 -0500

    Add di comparison
    Haven't tested this yet..

---
+++ example/di_comparison.py
@@ -0,0 +1,46 @@
+import di
+import os
+
+
+container = di.Container()
+
+
+class ConfigReader:
+    def get_config(self):
+        pass
+
+
+class EnvironmentConfigReader(ConfigReader):
+    def get_config(self):
+        return {
+            "logging": {"level": os.environ.get("LOGGING_LEVEL", "debug")},
+            "greeting": os.environ.get("GREETING", "Hello world"),
+        }
+
+
+container.bind(di.Dependant(EnvironmentConfigReader), ConfigReader)
+
+
+class Greeter:
+    def greet(self):
+        pass
+
+
+class ConsoleGreeter(Greeter):
+    def __init__(self, config_reader: ConfigReader):
+        self.config = config_reader.get_config()
+
+    def greet(self):
+        print(self.config["greeting"])
+
+container.bind(di.Dependant(ConsoleGreeter), Greeter)
+
+solved_greeter = container.solve(di.Dependant(Greeter))
+
+async def di_main():
+    greeter = await container.execute_solved(solved_greeter)
+    assert isinstance(greeter, ConsoleGreeter)
+
+
+if __name__ == "__main__":
+    asyncio.run(di_main())
